686342e7f9b599143dc2e1d551cb675147046aed
domi41 <dominique.merle.41@gmail.com>
2021-06-17 10:07:45 +0200

    feat: add custom Exceptions

---
+++ src/main/java/fr/mieuxvoter/mj/InvalidTallyException.java
@@ -0,0 +1,12 @@
+package fr.mieuxvoter.mj;
+
+import java.security.InvalidParameterException;
+
+/**
+ * Raised when the provided tally is invalid.
+ */
+class InvalidTallyException extends InvalidParameterException {
+
+	private static final long serialVersionUID = 3033391835216704620L;
+
+}

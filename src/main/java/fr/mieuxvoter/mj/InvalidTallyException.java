5dd6e83e8f76b97928bd60206e330badd521786d
domi41 <dominique.merle.41@gmail.com>
2025-02-17 05:19:49 +0100

    fix: shorten the inheritance chain of exceptions

--- src/main/java/fr/mieuxvoter/mj/InvalidTallyException.java
+++ src/main/java/fr/mieuxvoter/mj/InvalidTallyException.java
@@ -1,9 +1,7 @@
 package fr.mieuxvoter.mj;
 
-import java.security.InvalidParameterException;
-
 /** Raised when the provided tally is invalid. */
-class InvalidTallyException extends InvalidParameterException {
+class InvalidTallyException extends RuntimeException {
 
     private static final long serialVersionUID = 3033391835216704620L;
 }
